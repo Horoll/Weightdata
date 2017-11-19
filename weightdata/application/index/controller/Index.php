a2b466ef23a939334aa8addacc92f3199fbb8ffa
Horoll <horol_lkc@126.com>
2017-11-19 21:16:08 +0800

    2017-11-19 21:15

--- weightdata/application/index/controller/Index.php
+++ weightdata/application/index/controller/Index.php
@@ -10,7 +10,6 @@ class Index extends Controller
         return $this->fetch();
     }
 
-
     public function login($accountname,$password){
         $users = Db::table('user')->where('account',$accountname)->find();
         if(empty($users)) $this->error('该用户不存在');
@@ -28,4 +27,32 @@ class Index extends Controller
         if(empty(cookie('username'))) $this->error('请先登录','index/index');
         return $this->fetch();
     }
+
+    private $everyMeasure;//每次称重的数据集
+
+    protected $beforeActionList = [
+        'initDate'  =>  ['only'=>'dataCount,detailData'],
+    ];
+
+    //用于给everyMeasure属性赋值
+    private function initData(){
+        $everyMeasure =
+            Db::table('initial_data')
+                ->distinct(true)
+                ->field('sign')
+                ->select();
+        $this->everyMeasure = $everyMeasure;
+    }
+
+    //所有数据，按照每一次测量来分类
+    public function dataCount(){
+        return count($this->everyMeasure);
+    }
+
+
+    //查询详细的数据图表
+    public function detailData(){
+
+    }
+
 }
